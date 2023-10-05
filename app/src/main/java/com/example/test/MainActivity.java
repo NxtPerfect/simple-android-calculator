0351e4acaa63609179f6b05e36e76b61d6c07dcc
NxtPerfect <alakaxan@protonmail.com>
2023-10-05 17:36:08 +0200

    Initial commit

---
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void changeText() {
+        final TextView changeTextView = (TextView) findViewById(R.id.text_to_change);
+        Button changeTextButton = (Button) findViewById(R.id.button1);
+
+        changeTextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeTextView.setText("Changed");
+            }
+        });
+    }
+}
